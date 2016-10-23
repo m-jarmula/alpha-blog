e5d084c941f77863dee1278b818ba9cc1dd1f400
Maciek <maciej.jarmula@gmail.com>
2016-10-23 20:08:49 +0200

    editing

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,9 +7,26 @@ class ArticlesController < ApplicationController
     @article = Article.new
   end
   
+  def show
+    @article = Article.find(params[:id])
+  end
+  
+  def edit
+    @article = Article.find(params[:id])
+  end    
+  
   def create
     @article = Article.new(article_params params)
-    @article.save
+    if @article.save
+      flash[:notice] = "Article was created"
+      redirect_to article_path(@article)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    Article.find(params[:id]).destroy
     redirect_to articles_path
   end
   
