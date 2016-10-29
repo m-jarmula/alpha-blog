0bdedf0ee9828342c658003297fa6a2b32d410f6
Maciek <maciej.jarmula@gmail.com>
2016-10-29 23:57:41 +0200

    article author - current user

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -28,6 +28,7 @@ class ArticlesController < ApplicationController
   
   def create
     @article = Article.new(article_params)
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was created"
       redirect_to article_path(@article)
