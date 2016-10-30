9f9004bbf338f56908f5a716777aa67ba9a35566
Maciek <maciej.jarmula@gmail.com>
2016-10-30 08:48:04 +0100

    login after create nw user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,8 +13,9 @@ class UsersController < ApplicationController
   def create
     @user = User.create(user_params)
     if @user.save
+      session[:user_id] = @user.id
       flash[:success] = "You hace been sugned up"
-      redirect_to articles_path
+      redirect_to user_path(@user)
     else
       render :new
     end
